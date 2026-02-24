facc4974441c36076178ca1151a9975cd2bd2022
unknown <huyhoan13032006@gmail.com>
2026-02-24 20:56:26 +0700

    up

---
+++ Session11/src/business/Assassin.java
@@ -0,0 +1,52 @@
+package business;
+
+import model.ISkill;
+
+public class Assassin extends GameCharacter implements ISkill {
+    private int energy;
+
+    public Assassin(String name, int hp, int attackPower, int energy) {
+        super(name, hp, attackPower);
+        this.energy = energy;
+    }
+
+    public Assassin(int energy) {
+        this.energy = energy;
+    }
+
+    @Override
+    public void attack(GameCharacter target) {
+        if (this.energy >= 50){
+            target.takeDamage(super.getAttackPower());
+            energy -= 50;
+            System.out.printf("%s sử dụng nắm đấp sấm sét vào %s ." , super.getName() , target.getName());
+        } else {
+            target.takeDamage(super.getAttackPower()/2);
+            System.out.printf("%s sử dụng nắm đấp sấm sét vào %s ." , super.getName() , target.getName());
+        }
+    }
+
+    @Override
+    public void useUltimate(GameCharacter target) {
+        if (this.energy >= 100){
+            if (target.getHp() <= 1000) {
+                int damage = target.getHp();
+                target.takeDamage(damage);
+                this.energy -= 100;
+                System.out.printf("%s sử dụng One shot one kill vào %s .\n" , super.getName() , target.getName());
+                this.setHp(this.getHp() + damage);
+            } else {
+                target.takeDamage(super.getAttackPower() * 3);
+                this.energy -= 100;
+                System.out.printf("%s sử dụng cú bắn nem chua vào %s .\n" , super.getName() , target.getName());
+            }
+        } else {
+            System.out.printf("Năng lượng của %s không đủ để dùng chiêu cuối !.\n" , super.getName());
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("|Name : %-10s | HP : %-5d | Energy : %d |\n",super.getName(),super.getHp(),this.energy);
+    }
+}
---
+++ Session11/src/business/Chronomancer.java
@@ -0,0 +1,56 @@
+package business;
+
+import model.ISkill;
+
+public class Chronomancer extends GameCharacter implements ISkill {
+
+    private int timeEnergy;
+
+    public Chronomancer(String name, int hp, int attackPower, int timeEnergy) {
+        super(name, hp, attackPower);
+        this.timeEnergy = timeEnergy;
+    }
+
+    public Chronomancer(int timeEnergy) {
+        this.timeEnergy = timeEnergy;
+    }
+
+    @Override
+    public void attack(GameCharacter target) {
+        if (this.timeEnergy >= 30) {
+            target.takeDamage(super.getAttackPower() + 20);
+            this.timeEnergy -= 30;
+
+            System.out.println(super.getName() + " làm chậm thời gian của " + target.getName());
+        } else {
+            target.takeDamage(super.getAttackPower());
+            System.out.println(super.getName() + " làm chậm thời gian của " + target.getName());
+        }
+    }
+
+    @Override
+    public void useUltimate(GameCharacter target) {
+        if (this.timeEnergy >= 120) {
+
+            int damage = super.getAttackPower() * 4;
+            target.takeDamage(damage);
+
+            int heal = damage * 30 / 100;
+            this.setHp(super.getHp() + heal);
+
+            this.timeEnergy -= 120;
+
+            System.out.println(super.getName() + " đóng băng thời gian! Gây " + damage + " damage lên " + target.getName() + " và hồi " + heal + " HP cho bản thân.");
+        } else {
+            System.out.println(super.getName() + " không đủ Time Energy để dùng chiêu cuối!");
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("|Name : %-12s | HP : %-5d | TimeEnergy : %d |\n",
+                super.getName(),
+                super.getHp(),
+                this.timeEnergy);
+    }
+}
